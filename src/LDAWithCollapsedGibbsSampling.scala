edc4bda6e1ead81d5d1eefec006b6800e4947ef7
lvilnis <lvilnis@gmail.com>
2012-04-27 23:55:44 -0400

    Clean up LDA comments, variable names, formatting

--- src/LDAWithCollapsedGibbsSampling.scala
+++ src/LDAWithCollapsedGibbsSampling.scala
@@ -22,35 +22,37 @@ object LDAWithCollapsedGibbsSampling {
       Array.tabulate(doc.length) { wi => distinctWordToIndex(doc(wi)) }
     }
 
-    // alpha is the prior distribution over topics (whatever this means...)
-    // beta is the prior distribution over words (whatever this means...)
-    // gamma is the component value of beta since we're using flat prior
+    // alpha is the prior distribution over topics
+    // beta is the prior distribution over words
+    // gamma is the component value of beta since we're using flat priors
     val gamma = 0.1
     val alpha = Array.fill(K)(50 / K)
     val beta = Array.fill(J)(gamma)
 
-    // z(m, n) is the topic of the n'th word in the m'th document
-    // c(k, m, j) is number of times word j is assigned to topic k in document m
-
-    // use a uniform random initial assignment of topic to each word z(_, _)
+    // z(a, b) is the topic of the b'th word in the a'th document
+    // use a uniform random initial assignment of topic to each word
     val z = Array.tabulate(M) { di =>
       Array.fill(documents(di).length) { scala.util.Random.nextInt(K) }
     }
-    // first fill c(_, _, _) with zeroes
+
+    // c(k, m, j) is number of times word j is assigned to topic k in document m
     val c = Array.fill(K, M, J)(0)
 
-    val cOverWords = Array.fill(K, M)(0)
-    val cOverDocs = Array.fill(K, J)(0)
-    val cOverWordsAndDocs = Array.fill(K)(0)
+    // cj(k, m) is number of times any word is assigned topic k in document m
+    // cm(k, j) is number of times word j is assigned topic k
+    // cmj(k) is number of times any word is assigned topic k
+    val cj = Array.fill(K, M)(0)
+    val cm = Array.fill(K, J)(0)
+    val cmj = Array.fill(K)(0)
 
     @inline def updateTopicCounts(topic: Int, doc: Int, word: Int, update: Int): Unit = {
-      cOverWords(topic)(doc) += update
-      cOverDocs(topic)(word) += update
-      cOverWordsAndDocs(topic) += update
+      cj(topic)(doc) += update
+      cm(topic)(word) += update
+      cmj(topic) += update
       c(topic)(doc)(word) += update
     }
 
-    // update the word counts in c(_, _, _) according to our initial z(_, _):
+    // update the word counts in c according to our initial z:
     for (doc <- 0 until M; docWord <- 0 until z(doc).length) {
       val topic = z(doc)(docWord)
       val word = y(doc)(docWord)
@@ -66,43 +68,33 @@ object LDAWithCollapsedGibbsSampling {
 
       val word = y(doc)(docWord)
 
-      // first, we decrement the count in c to exclude the current doc-word
-      // we should pull these out to methods and see if they in-line... my guess is no because they're closures
+      // first, we decrement the counts to exclude the current doc-word
       val oldTopic = z(doc)(docWord)
       updateTopicCounts(oldTopic, doc, word, -1)
 
       // now we determine the likelihood that the current doc-word has each topic
-      // now for each doc-word (a, b), we calculate the most likely topic given all the
-      // other topic assignments
+      // given all the other topic assignments
       var topic = 0
       while (topic < numTopics) {
         topicLikelihoods(topic) =
-          (cOverWords(topic)(doc) + alpha(topic)) *
-          (cOverDocs(topic)(word) + beta(word)) /
-          (cOverWordsAndDocs(topic) + gamma * J)
+          (cj(topic)(doc) + alpha(topic)) *
+          (cm(topic)(word) + beta(word)) /
+          (cmj(topic) + gamma * J)
         topic += 1
       }
 
-      //println(topicLikelihoods.toList)
-
-      // find the most likely topic assignment for the current doc-word
-      // pick the next topic assignment according to the probabilities
-      val newTopic: Int = pickTopic(topicLikelihoods)
+      // pick the next topic assignment according to the likelihoods
+      val newTopic = pickTopic(topicLikelihoods)
 
       z(doc)(docWord) = newTopic
       updateTopicCounts(newTopic, doc, word, 1)
     }
 
-    // now, print out all the topics:
-
+    // now, group all the words into topics
     val topicWordPairs = z.zipWithIndex.toSeq flatMap { case (doc, d) =>
       doc.zipWithIndex map { case (t, w) => (t, distinctWords(y(d)(w))) }
     }
-
     val topics = topicWordPairs groupBy { _._1 } mapValues { _ map { _._2 } }
-
-    println(topics)
-
     topics.toSeq map { _._2 }
   }
 
