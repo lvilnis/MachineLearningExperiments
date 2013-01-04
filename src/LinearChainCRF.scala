27f997286f9d6617bdbfd64d1ea4a987e88289db
lvilnis <lvilnis@gmail.com>
2013-01-04 10:36:22 -0500

    added learning and marginals

--- src/LinearChainCRF.scala
+++ src/LinearChainCRF.scala
@@ -30,13 +30,16 @@ object Domain {
 }
 
 object LinearChainCRF {
-  case class Observation(obs: Seq[Int])
-  case class Label(labels: Seq[Int])
-  case class Instance(obs: Observation, labels: Label)
-  case class Weights(obs: Seq[Double], markov: Seq[Double] /*, bias: Seq[Double] */)
-  case class ObsMarkovFactor(obsIndex: Int, obs: Int, markov: Int)
+  final case class Observation(obs: Seq[Int])
+  final case class Label(labels: Seq[Int])
+  final case class Instance(obs: Observation, labels: Label)
+  final case class Weights(obs: Array[Double], markov: Array[Double] /*, bias: Seq[Double] */)
+  case class ObsFactor(obsIndex: Int, obs: Int, markov: Int)
   case class BiasFactor(labelIndex: Int, label: Int)
   case class MarkovFactor(leftLabelIndex: Int, rightLabelIndex: Int, leftLabel: Int, rightLabel: Int)
+  final case class Marginal(probs: IndexedSeq[Array[Array[Double]]]) {
+    def prob(obs: Int, leftLabel: Int, rightLabel: Int): Double = probs(obs)(leftLabel)(rightLabel)
+  }
 
   def main(args: Array[String]): Unit = {
     // Do this stuff later, assume we're given labeled instances
@@ -52,7 +55,7 @@ object LinearChainCRF {
     val markovDomain = Domain()
     words.foreach(obsDomain.indexForWord(_))
     labels.foreach(markovDomain.indexForWord(_))
-    val markovWeights = Array(10.0, 2.0, 10.0, 2.0)
+    val markovWeights = Array(10.0, 2.0, 2.0, 10.0)
     val obsWeights = Array(10.0, 1.0, 1.0, 1.0, 10.0, 15.0)
     val weights = Weights(obsWeights, markovWeights)
     val hiddenLabels = Seq("GOOD","GOOD","GOOD","GOOD","GOOD","GOOD","BAD","BAD","BAD","BAD","BAD", "GOOD", "GOOD")
@@ -61,17 +64,39 @@ object LinearChainCRF {
     val generatedWords = hiddenLabelIndices.map(generateWord)
     println(hiddenLabels)
     println(generatedWords.map(obsDomain.wordForIndex(_)))
-    val mapEstimate = infer(Observation(generatedWords), weights, markovDomain, obsDomain)
+    val mapEstimate = new Infer(weights, markovDomain, obsDomain).inferViterbi(Observation(generatedWords))
     println(mapEstimate.labels.map(markovDomain.wordForIndex(_)))
   }
 
-  def learnWeights(instances: Seq[Instance], labelDomain: Domain, obsDomain: Domain): Weights = {
-    sys.error("unimpl")
+  class Learn(labelDomain: Domain, obsDomain: Domain, sgdPasses: Int = 5, learningRate: Double = 1.0, l2: Double = 0.01, initialState: Int = 0) {
+    def learnWeights(instances: Seq[Instance]): Weights = {
+      val weights = Weights(Array.fill(labelDomain.size * obsDomain.size)(0.0), Array.fill(labelDomain.size * labelDomain.size)(0.0))
+      val infer = new Infer(weights, labelDomain, obsDomain)
+      for (p <- 1 to sgdPasses; inst <- instances) {
+        val marginal = infer.inferForwardBackward(inst.obs)
+        val markovGradient = Array.fill(labelDomain.size * labelDomain.size)(0.0)
+        val obsGradient = Array.fill(labelDomain.size * obsDomain.size)(0.0)
+        for (o <- 0 until inst.obs.obs.size; i <- 0 until labelDomain.size; j <- 0 until labelDomain.size) {
+          val curObs = inst.obs.obs(o)
+          val curObsGradIdx = j * obsDomain.size + curObs
+          val curMarkovGradIdx = j * labelDomain.size + i
+          obsGradient(curObsGradIdx) -= math.exp(marginal.prob(curObs, i, j))
+          markovGradient(curMarkovGradIdx) -= math.exp(marginal.prob(curObs, i, j))
+          if ((o == 0 && i == initialState && j == inst.labels.labels(o)) ||
+              (i == inst.labels.labels(o - 1) && j == inst.labels.labels(o))) {
+            obsGradient(curObsGradIdx) += 1
+            markovGradient(curMarkovGradIdx) += 1
+          }
+        }
+        // add by unregularized SGD for now
+        for (i <- 0 until markovGradient.size) weights.markov(i) += (markovGradient(i) - l2) * learningRate
+        for (i <- 0 until obsGradient.size) weights.obs(i) += (obsGradient(i) - l2) * learningRate
+      }
+      weights
+    }
   }
 
-  def infer(obs: Observation, weights: Weights, labelDomain: Domain, obsDomain: Domain): Label = {
-    val initialState = 0
-
+  class Infer(weights: Weights, labelDomain: Domain, obsDomain: Domain, initialState: Int = 0) {
     @inline def score(leftLabel: Int, rightLabel: Int, rightObs: Int): Double = {
       val markovScore = weights.markov(labelDomain.size * leftLabel + rightLabel)
       val obsScore = weights.obs(obsDomain.size * rightLabel + rightObs)
@@ -80,24 +105,33 @@ object LinearChainCRF {
 
     @inline def overStates[A: Manifest](fun: Int => A): Array[A] = Array.tabulate(labelDomain.size)(fun)
 
-    def calculateNextForward(lastFwd: Array[Double], nextObs: Int): Array[Double] =
-      overStates(j => fastSum(overStates(i => score(i, j, nextObs) + lastFwd(i))))
-    val initialFwd = overStates(i => score(initialState, i, obs.obs(0)))
-    val forwardVars = obs.obs.scanLeft(initialFwd)(calculateNextForward)
+    def inferForwardBackward(obs: Observation): Marginal = {
+      def calculateNextForward(lastFwd: Array[Double], nextObs: Int): Array[Double] =
+        overStates(j => fastSum(overStates(i => score(i, j, nextObs) + lastFwd(i))))
+      val initialFwd = overStates(i => score(initialState, i, obs.obs(0)))
+      val forwardVars = obs.obs.scanLeft(initialFwd)(calculateNextForward)
 
-    def calculateLastBackward(lastObs: Int, nextBackward: Array[Double]): Array[Double] =
-      overStates(i => fastSum(overStates(j => score(i, j, lastObs) + nextBackward(j))))
-    val initialBwd = overStates(_ => 1.0)
-    val backwardVars = obs.obs.scanRight(initialBwd)(calculateLastBackward)
+      def calculateLastBackward(lastObs: Int, nextBackward: Array[Double]): Array[Double] =
+        overStates(i => fastSum(overStates(j => score(i, j, lastObs) + nextBackward(j))))
+      val initialBwd = overStates(_ => 0.0)
+      val backwardVars = obs.obs.scanRight(initialBwd)(calculateLastBackward)
 
-    def calculateNextViterbi(lastViterbi: Array[(Double, Int)], nextObs: Int): Array[(Double, Int)] =
-      overStates(j => overStates(i => (score(i, j, nextObs) + lastViterbi(i)._1, i)).maxBy(_._1))
-    val initialViterbi = initialFwd.map((_, -1))
-    val viterbis = obs.obs.scanLeft(initialViterbi)(calculateNextViterbi)
-    val lastV = viterbis.last
-    val restV = viterbis.drop(2)
-    val (_, finalBackpointer) = lastV.maxBy(_._1)
-    val mapAssignment = restV.scanRight(finalBackpointer)((arr, ptr) => arr(ptr)._2)
-    Label(mapAssignment)
+      val marginals = for (o <- 1 until forwardVars.size) yield
+        overStates(i => overStates(j => forwardVars(o - 1)(i) * score(i, j, obs.obs(o)) * backwardVars(o)(j)))
+
+      Marginal(marginals)
+    }
+
+    def inferViterbi(obs: Observation): Label = {
+      def calculateNextViterbi(lastViterbi: Array[(Double, Int)], nextObs: Int): Array[(Double, Int)] =
+        overStates(j => overStates(i => (score(i, j, nextObs) + lastViterbi(i)._1, i)).maxBy(_._1))
+      val initialViterbi = overStates(i => (score(initialState, i, obs.obs(0)), -1))
+      val viterbis = obs.obs.scanLeft(initialViterbi)(calculateNextViterbi)
+      val lastV = viterbis.last
+      val restV = viterbis.drop(2)
+      val (_, finalBackpointer) = lastV.maxBy(_._1)
+      val mapAssignment = restV.scanRight(finalBackpointer)((arr, ptr) => arr(ptr)._2)
+      Label(mapAssignment)
+    }
   }
 }
